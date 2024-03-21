3177880f493d973d0f7243db7f985eefd2fb446c
Rodrick Gamwo <rtg18@pitt.edu>
2024-03-21 10:52:06 -0400

    new changes

--- passwords.sql
+++ passwords.sql
@@ -1,1 +1,13 @@
-SELECT NOW()
+-- Active: 1711031920597@@127.0.0.1@5432@passwords@public
+-- Create the database
+CREATE DATABASE password_manager;
+
+-- Connect to the newly created database
+
+
+-- Create a table to store user credentials
+CREATE TABLE users (
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(50) UNIQUE NOT NULL,
+   password VARCHAR(255) NOT NULL
+);
